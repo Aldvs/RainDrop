822a46974912d8f04b46cde8d0e7bd8fd70379e6
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-23 22:57:57 +0300

    Initial Commit

---
+++ RainDrop/RainDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainDropApp.swift
+//  RainDrop
+//
+//  Created by admin on 23.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RainDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
